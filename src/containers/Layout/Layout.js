f70a770de164692b132d0195b6153a2720acaeb4
Ivan Gavrilov <gavrilov.iv.spb@gmail.com>
2022-01-19 10:51:58 +0300

    Основное меню на главной

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { ScreenClassProvider } from "react-grid-system";
 // import { Header } from "components";
 
 function Layout() {
   return (
-    <>
+    <ScreenClassProvider>
       {/* <Header /> */}
       <main>
         <Outlet />
       </main>
-    </>
+    </ScreenClassProvider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,17 @@
-import React, { Suspense } from "react";
+import { Suspense, lazy as RtLazy } from "react";
 
 import { useTranslation, Trans } from "react-i18next";
 import cx from "classnames";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Container } from "react-grid-system";
 
-import CSelect from "components/CSelect";
+// import CSelect from "components/CSelect";
 import styles from "styles/home.module.css";
 
 import cubStylesBg from "styles/cub.background.module.css";
 
-const LayoutBackground = React.lazy(() => import("components/LayoutBackground"));
+const LayoutBackground = RtLazy(() => import("components/LayoutBackground"));
 
 function Home() {
   const { t } = useTranslation();
@@ -33,73 +34,82 @@ function Home() {
           </div>
         </LayoutBackground>
       </Suspense>
-      <>
+      <div className={styles.content_home}>
         <div className={styles.cover_overlay} />
-        {/* <div className={styles.home_container}> */}
-        <div className={cx(styles.block_decoration, styles.main_block)}>
-          <div className={styles.mb_container}>
-            <h1>
-              <Trans i18nKey="home.article.part1">
-                <span>Hi, I'm</span> Ivan Gavrilov
-              </Trans>
-            </h1>
-            <h2>Designer &amp; Developer</h2>
+        <Container fluid>
+          {/* <Row> */}
+          <div className={cx(styles.block_decoration, styles.main_block)}>
+            <div className={styles.mb_container}>
+              <h1>
+                <Trans i18nKey="home.article.part1">
+                  <span>Hi, I'm</span> Ivan Gavrilov
+                </Trans>
+              </h1>
+              <h2>Designer &amp; Developer</h2>
+            </div>
           </div>
-        </div>
-        <div className={styles.menu_blocks}>
-          <Link to="about/" title={t("mainMenu.about")}>
-            <span
-              className={cx(styles.block_decoration, styles.menu_block, styles.menu_block__about)}
-            >
-              <span className={styles.block_container}>
-                <h2 className={styles.block_container_item}>{t("mainMenu.about")}</h2>
+          <div className={styles.menu_blocks}>
+            <Link to="about/" title={t("mainMenu.about")}>
+              <span
+                className={cx(styles.block_decoration, styles.menu_block, styles.menu_block__about)}
+              >
+                <span className={styles.block_container}>
+                  <h2 className={styles.block_container_item}>{t("mainMenu.about")}</h2>
+                </span>
               </span>
-            </span>
-          </Link>
-          <Link to="portfolio/" title={t("mainMenu.portfolio")}>
-            <span
-              className={cx(
-                styles.block_decoration,
-                styles.menu_block,
-                styles.menu_block__portfolio
-              )}
-            >
-              <span className={styles.block_container}>
-                <h2 className={styles.block_container_item}>{t("mainMenu.portfolio")}</h2>
+            </Link>
+            <Link to="portfolio/" title={t("mainMenu.portfolio")}>
+              <span
+                className={cx(
+                  styles.block_decoration,
+                  styles.menu_block,
+                  styles.menu_block__portfolio
+                )}
+              >
+                <span className={styles.block_container}>
+                  <h2 className={styles.block_container_item}>{t("mainMenu.portfolio")}</h2>
+                </span>
               </span>
-            </span>
-          </Link>
-          <Link to="skills/" title={t("mainMenu.skills")}>
-            <span
-              className={cx(styles.block_decoration, styles.menu_block, styles.menu_block__skills)}
-            >
-              <span className={styles.block_container}>
-                <h2 className={styles.block_container_item}>{t("mainMenu.skills")}</h2>
+            </Link>
+            <Link to="skills/" title={t("mainMenu.skills")}>
+              <span
+                className={cx(
+                  styles.block_decoration,
+                  styles.menu_block,
+                  styles.menu_block__skills
+                )}
+              >
+                <span className={styles.block_container}>
+                  <h2 className={styles.block_container_item}>{t("mainMenu.skills")}</h2>
+                </span>
               </span>
-            </span>
-          </Link>
-          <Link to="contact/" title={t("mainMenu.contact")}>
-            <span
-              className={cx(styles.block_decoration, styles.menu_block, styles.menu_block__contact)}
-            >
-              <span className={styles.block_container}>
-                <h2 className={styles.block_container_item}>{t("mainMenu.contact")}</h2>
+            </Link>
+            <Link to="contact/" title={t("mainMenu.contact")}>
+              <span
+                className={cx(
+                  styles.block_decoration,
+                  styles.menu_block,
+                  styles.menu_block__contact
+                )}
+              >
+                <span className={styles.block_container}>
+                  <h2 className={styles.block_container_item}>{t("mainMenu.contact")}</h2>
+                </span>
               </span>
-            </span>
-          </Link>
-        </div>
-        <div>
-          <div className={cx(styles.block_decoration, styles.empty)} />
-          <div className={cx(styles.block_decoration, styles.empty_2)}>
-            {/* <span className={styles.block_container}> */}
-            {/* <h2 className={styles.block_container_item}> */}
-            {/* <CSelect /> */}
-            {/* </h2> */}
-            {/* </span> */}
+            </Link>
           </div>
-        </div>
-        {/* </div> */}
-      </>
+          <div>
+            <div className={cx(styles.block_decoration, styles.empty)} />
+            <div className={cx(styles.block_decoration, styles.empty_2)}>
+              {/* <span className={styles.block_container}> */}
+              {/* <h2 className={styles.block_container_item}> */}
+              {/* <CSelect /> */}
+              {/* </h2> */}
+              {/* </span> */}
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 }
