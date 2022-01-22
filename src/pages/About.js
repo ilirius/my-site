7e3ae09b1d56d65387e37fd4438f69655d766b76
Ivan Gavrilov <gavrilov.iv.spb@mail.ru>
2022-01-22 20:18:45 +0300

    Верстка облако "Мои интересы"
    - Давлена облако тегов с интересами
    - Фикс аватарки

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,12 +3,34 @@ import cx from "classnames";
 import { Container, Row, Col } from "react-grid-system";
 import { useTranslation } from "react-i18next";
 
+// images
+import { ReactComponent as SpeedBlackIcon } from "assets/svg/speed_black_24dp.svg";
+import { ReactComponent as CellphoneIcon } from "assets/svg/cellphone-link.svg";
+import { ReactComponent as LightbulbIcon } from "assets/svg/lightbulb-outline.svg";
+import { ReactComponent as RocketIcon } from "assets/svg/rocket-launch-outline.svg";
 import avatars from "assets/jpg/61w3aNNXTww-350.jpg";
 
 import "styles/about.background.scss";
 import "styles/about.css";
+import "styles/cloud-tags-anim.scss";
+
+// lazy component
 
 const LayoutBackground = lazy(() => import("components/LayoutBackground"));
+const TagItem = lazy(() => import("components/TagItem"));
+
+const tagList = [
+  "Programming",
+  "Cinema",
+  "Graphics",
+  "Cars",
+  "Fantasy",
+  "Fantastic",
+  "IT",
+  "Design",
+  "Photography",
+  "Mythology",
+];
 
 function About() {
   const { t } = useTranslation();
@@ -31,51 +53,74 @@ function About() {
         </LayoutBackground>
       </Suspense>
       {/* <Container fluid> */}
-      <Container>
-        <h1 className="block-title">{t("about.pageHeader")}</h1>
-        <Row>
-          <Col xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Row>
-              <Col xl={6} lg={6} md={6} sm={6} xs={12} className="label-wrap">
-                <div className="label-box" />
-                <h2>Fast</h2>
-                <p>Fast load times and lag free interaction, my highest priority.</p>
-              </Col>
-              <Col xl={6} lg={6} md={6} sm={6} xs={12} className="label-wrap">
-                <div className="label-box" />
-                <h2>Responsive</h2>
-                <p>My layouts will work on any device, big or small.</p>
-              </Col>
-            </Row>
-          </Col>
-          <Col xl={6} lg={6} md={6} sm={12} xs={12}>
-            <Row>
-              <Col xl={6} lg={6} md={6} sm={6} xs={12} className="label-wrap">
-                <div className="label-box" />
-                <h2>Intuitive</h2>
-                <p>Strong preference for easy to use, intuitive UX/UI.</p>
-              </Col>
-              <Col xl={6} lg={6} md={6} sm={6} xs={12} className="label-wrap">
-                <div className="label-box" />
-                <h2>Dynamic</h2>
-                <p>Websites don't have to be static, I love making pages come to life.</p>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row className="skills-wrapper">
-          <Col xs={5} md={5}>
-            <img src={avatars} alt="Avatars" />
-            <h3>Кто этот парень?</h3>
-            {/* Who's this guy? */}
-            <p>{t("about.article.part1")}</p>
-            <p>{t("about.article.part2")}</p>
-          </Col>
-          <Col xs={7} md={7}>
-            skills
-          </Col>
-        </Row>
-      </Container>
+      <div className="about-container">
+        <Container>
+          <h1 className="block-title">{t("about.pageHeader")}</h1>
+          <Row>
+            <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+              <Row>
+                <Col xl={6} lg={6} md={6} sm={6} xs={6} className="label-wrap">
+                  <div className="label-box">
+                    <SpeedBlackIcon className="svg-icon" />
+                  </div>
+                  <h2>Fast</h2>
+                  <p>Fast load times and lag free interaction, my highest priority.</p>
+                </Col>
+                <Col xl={6} lg={6} md={6} sm={6} xs={6} className="label-wrap">
+                  <div className="label-box">
+                    <CellphoneIcon className="svg-icon" />
+                  </div>
+                  <h2>Responsive</h2>
+                  <p>My layouts will work on any device, big or small.</p>
+                </Col>
+              </Row>
+            </Col>
+            <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+              <Row>
+                <Col xl={6} lg={6} md={6} sm={6} xs={6} className="label-wrap">
+                  <div className="label-box">
+                    <LightbulbIcon className="svg-icon" />
+                  </div>
+                  <h2>Intuitive</h2>
+                  <p>Strong preference for easy to use, intuitive UX/UI.</p>
+                </Col>
+                <Col xl={6} lg={6} md={6} sm={6} xs={6} className="label-wrap">
+                  <div className="label-box">
+                    <RocketIcon className="svg-icon" />
+                  </div>
+                  <h2>Dynamic</h2>
+                  <p>Websites don't have to be static, I love making pages come to life.</p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="info-wrapper">
+            <Col xs={12} md={5} className="info">
+              <div className="avatars">
+                <img src={avatars} alt="Avatars" className="avatars__img" />
+              </div>
+              <h3>Кто этот парень?</h3>
+              {/* Who's this guy? */}
+              <p>{t("about.article.part1")}</p>
+              <span>{t("about.article.part2")}</span>
+            </Col>
+            <Col xs={12} md={7}>
+              {/* Факты обо мне */}
+              <h3>Мои интересы</h3>
+              <dl className="sidebar-block sidebar-tags tags-cloud">
+                <dt>Tags</dt>
+                <dd className="tags">
+                  <Suspense fallback={<div>Loading...</div>}>
+                    {tagList.map((val, key) => (
+                      <TagItem text={t(`about.tagsCloud.${val}`)} key={`tag-${key}`} />
+                    ))}
+                  </Suspense>
+                </dd>
+              </dl>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
